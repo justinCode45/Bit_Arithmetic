b9f32d3a22c857db9e2871d6cf684b7d462286b9
justinCode45 <81457595+justinCode45@users.noreply.github.com>
2021-12-12 18:49:01 +0800

    Update SerialAdder.h

--- SerialAdder.h
+++ SerialAdder.h
@@ -1,30 +1,26 @@
-
-#include <iostream>
-using namespace std ;
-int adder(int& a,int& b){
+int adder(int &a,int& b){
     //串行進位全加法器
-    int an=0;
+    int ans=0;
     int cin=0;
     int power=0;
     while (a>0 or b>0){
         if ((a%2^b%2^cin)!=0){
-            an=(an|(1<<power));
+            ans=(ans|(1<<power));
         }
         cin= (a%2&b%2|(a%2^b%2)&cin);
         a>>=1;
         b>>=1;
         int m=1;
-        while( power & m ){
-            power = power ^ m;
-            m <<= 1;
+        while(power&m){
+            power=power^m;
+            m<<=1;
         }
-        power = power ^ m;
+        power = power^m;
     }
     if (cin !=0){
-        an=(an|(1<<power));
+        ans=(ans|(1<<power));
     }
-    return an;
+    return ans;
 }
-
 
 
